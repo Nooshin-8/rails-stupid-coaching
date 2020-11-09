c43126b51fb100a1099fbbb17dfdb2f25fc92a69
Nooshin Attarian <noshinatarian@gmail.com>
2020-11-09 14:14:48 -0500

    stupid coach

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,18 @@ class PagesController < ApplicationController
   end
 
   def ask
-    # search = params[:ourdata]
   end
 
   def answer
+    @user_question = params[:question]
+     if @user_question == "I am going to work"
+      @answer = "Great!"
+     elsif @user_question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+     else
+      @answer = "I don't care, get dressed and go to work!"
+     end
   end
-
 end
 
 # If the message is I am going to work, the coach will answer Great!
